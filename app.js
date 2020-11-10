9cd4a62174b4103468d50332fb7200d4b56d7dd3
Wahid Hashoory <wahidhashoory@Wahids-MacBook-Pro.local>
2020-11-10 00:46:38 -0600

    Install nodemon and express

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-// const {sum} = require ("./helpers"); 
-
-// const http = require("http");
-
-// const server = http.createServer((req, res)=>{
-//     res.end("hello from node JS, I was born today!");
-// })
-// server.listen(30)
-// const total = sum(2,208);
-// console.log("TOTAL",  total);
-// console.log('http://localhost:30');
+const express = require('express');
+const app = express();
 
 
-//express 
-// const express = require('express');
-// const app = express();
-// app.get('/', (req, res) => {
-//     res.send("hey whats up from express, this is Wahid!!!, expess is so much fun");
-// });
-// app.listen(3000);
-
-
-
-const fs = require('fs');
-const fileName = "target.txt";
-
-const errHander = err => console.log(err);
-const dataHander = data => console.log(data.toString());
-
-fs.readFile(fileName, (err, data) =>  {
-    if(err){
-        errHander(err)
-       
-    }
-    dataHander(data);
-})
-console.log("node js async programming .. ?")
+app.get('/', (req, res) => {
+    res.send("hey whats up from express, this is Wahid!!!, expess is so much fun");
+});
+app.listen(3000);
 
 
 
